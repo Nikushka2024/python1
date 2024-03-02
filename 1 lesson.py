edc5635eaebc54da76e39d6548249de94e9b210a
Nikushka2024 <megrelynika@gmail.com>
2024-03-02 09:16:18 -0800

    check age script

---
+++ 1 lesson.py
@@ -0,0 +1,11 @@
+def koks(age):
+    # age = int(input())
+
+    if (age < 18):
+        print ('pishov na huy pizduk')
+    elif(age > 50):
+        print ("NONONO, YOU are too OLD, PEDIK")
+    else:
+        print ('namana')
+age = int(input())
+koks(age)
